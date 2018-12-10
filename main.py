acbf31cee82d2424b5e2e5bc0e615ca81d2c8dd2
Arnar Elvarsson <Arnar91@gmail.com>
2018-12-10 22:14:46 +0000

    Used os.walk to find all videofiles and appended filename + path into a list.

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 #main function
+#r"^.*S\d\dE\d\d" < possible show and episode regex
 
-def get_valid_file_types(files):
-    valid_type = ['avi', 'mkv', 'mp4']
+import os, re
+def get_valid_file_types(Directory):
+    #all valid video file types
+    valid_type =  ["m4v", "flv", "mpeg", "mov", "mpg","mpe", "wmv", "MOV", "mp4"] 
+    foundMatch = []
+    for dirName, subDirList, fileList in os.walk(Directory): #Walks the given directory and any subdirectory/ies
+        for fName in fileList:
+            if fName.split('.')[-1] in valid_type: #if the file ending matches any valid file ending, append to the list both the filename, and the full path to the file
+                #TODO match show name and season. store differently.
+                #possible solution for naming using https://github.com/divijbindlish/parse-torrent-name
+                #Requires teacher confirmation before we go further.
+                foundMatch.append((fName, os.path.join(dirName, fName)))
+                
     
-    filtered_accepetables = []
-    formatted_file_name = []
-    for item in files:
+    '''for item in Directory:
         temp = item.split('.')
         print(temp[-1])
         if temp[-1] in valid_type:
-            filtered_accepetables.append(item)
-
-
-    print(filtered_accepetables)
+            filtered_accepetables.append(item)'''
+    print(foundMatch)
     return None
 
-get_valid_file_types(['My Girl 1 1991 DvDrip[Eng]-greenbud1969.avi', 'Modern.Family.S07E05.720p.HDTV.x264-KILLERS.mkv', 'New.Girl.S03E03.720p.HDTV.X264-DIMENSION.mkv', 'A.Quiet.Place.2018.1080p.BluRay.x264.DD5.1.Isl.Texti.mkv.torrent' ])
+get_valid_file_types("downloads")
 
 def main_func(s):
     return None
