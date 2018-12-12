37e0715af7d4af768b63aefde13e4fcb98deca30
Arnar Elvarsson <Arnar91@gmail.com>
2018-12-12 14:12:08 +0000

    refactored get_valid_types, now calls a new function to sort the unsorted files

--- main.py
+++ main.py
@@ -1,57 +1,74 @@
 #main function
 
 import os, re, PTN
+
+def sort_Unordered(Unordered):
+    movies = []
+    tvShows = []
+    unknown = []
+    for tup in Unordered:
+        info = tup[0]
+        #print(info)
+        if 'episode' in info and 'season' not in info: #Default to Season 01
+            season = "S01"
+            episode = "E"+str(info['episode'])
+            title = info['title'].lower()
+            tvShows.append((title, season, episode, tup[1]))
+        elif 'season' and 'episode' in info:
+            season = "S"+ str(info['season'])
+            episode = "E"+str(info['episode'])
+            title = info['title'].lower()
+            tvShows.append((title, season, episode, tup[1]))
+        #Should have covered most of the tv-shows excluding oddly named ones
+        elif 'year' in info: 
+            title = info['title'].lower()
+            movies.append((title, tup[1]))
+        else: #these files require further checking, for now we put them into unknowns
+            unknown.append(tup)
+
+    return tvShows, movies, unknown
+
+
+
+
 def get_valid_file_types(Directory):
     #all valid video file types
     valid_type =  ["m4v", "flv", "mpeg", "mov", "mpg","mpe", "wmv", "MOV", "mp4"] 
     Unsorted = []
-    Movies = []
-    Shows = []
     fileCounter = 0
     for dirName, subDirList, fileList in os.walk(Directory): #Walks the given directory and any subdirectory/ies
         for fName in fileList:
             if fName.split('.')[-1] in valid_type: #only check if the file ends in a file-format we're looking for
                 fileCounter += 1 #counter to make sure we're listing every file we check
                 info = PTN.parse(fName) #extract all available information from filename via Parse-Torrent-Name library
-                if  'year' in info and 'season' and 'episode' not in info: #Catch most movies, identified by having year and title 
-                    path = os.path.join(dirName, fName)
-                    Movies.append((info, path))
-                elif 'episode' and 'season' in info: #No need to work these any further, keep the info we need and the path so we can move the file to the correct folder
-                    Season = "S"+ str(info['season'])
-                    Episode = "E"+str(info['episode'])
-                    title = info['title'].lower()
-                    path = os.path.join(dirName, fName)
-                    Shows.append((title, Season, Episode, path))
-                else: #anything that doesn't match the prior categories
-                    path = os.path.join(dirName, fName)
-                    Unsorted.append((info, path))
-
+                path = os.path.join(dirName, fName)
+                Unsorted.append((info, path))
+    tvShows, movies, unknown = sort_Unordered(Unsorted)
     #make sure we've sorted every single file applicable to the valid typing
-    listSum = len(Shows) + len(Unsorted) + len(Movies)
+    listSum = len(tvShows) + len(unknown) + len(movies)
     if fileCounter == listSum:
         pass
     else:
         print("We're missing some files captain")
         return
-    Other = [] #list of items we could not sort, sort into it's own folder and must be dealt with manually
+    '''Other = [] #list of items we could not sort, sort into it's own folder and must be dealt with manually
     for item in Unsorted:
         if re.fullmatch(r'\d\d\d', item[0]['title'].split(' ')[-1]):
             tmp = item[0]['title'].split(' ')
             title = ' '.join(tmp[0:len(tmp)-1]).lower()
             season = "S0"+ tmp[-1][0]
             Episode = "E"+ tmp[-1][1:]
-            Shows.append((title, season, Episode, item[1]))
+            tvShows.append((title, season, Episode, item[1]))
             Unsorted.remove(item) #not sure this currently does anything?
         else:
             #TODO find title and serie in directory path?
             #Set default season 01 if only r'E\d\d' in filename
             #further filtering
-            else:
-                Other.append(item)
+            Other.append(item)'''
     Unsorted.clear() #empty unsorted as we have no need for it further
-    #print(Movies)
-    #print(Shows)
-    #print(Other)
+    #print(movies)
+    #print(tvShows)
+    print(unknown)
     return None
 
 get_valid_file_types("downloads")
